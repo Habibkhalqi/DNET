5e073330fb303213e8bc26ae838e7a952e0da031
Habibullah <103850736+Habibkhalqi@users.noreply.github.com>
2024-11-22 02:45:30 +0500

    first code on rider

---
+++ library/static and abstract/abstract.cs
@@ -0,0 +1,12 @@
+namespace static_and_abstract;
+
+public abstract class animal
+{
+    public abstract void sound();
+
+    public void eat()
+    {
+        Console.WriteLine(" animal is  eating!");
+    }
+
+}
---
+++ library/static and abstract/creator.cs
@@ -0,0 +1,6 @@
+namespace static_and_abstract;
+
+public class creator: animal
+{
+    sound = "woo woo";
+}
